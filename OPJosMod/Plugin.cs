d504e18939eb102c75a2556b5adfc6cab2048e73
Jglauby <jglauby@gmail.com>
2024-01-06 13:34:18 -0600

    Add project files.

---
+++ OPJosMod/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPJosMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class OpJosMod : BaseUnityPlugin
+    {
+        private const string modGUID = "BigGayPeePeeSuck.OpJosMod";
+        private const string modName = "OpJosMod";
+        private const string modVersion = "1.0.0.0";
+
+
+
+    }
+}
