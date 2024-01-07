4799e53f58715b9a9e72b98f1a400b525b8b559a
Jglauby <jglauby@gmail.com>
2024-01-06 18:58:42 -0600

    built out infinite sprint

---
+++ OPJosMod/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,21 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPJosMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]//if can use typeof(PlayerControllerB.Update)
+        [HarmonyPostfix]
+        static void patchUpdate(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+    }
+}
--- OPJosMod/Plugin.cs
+++ OPJosMod/Plugin.cs
@@ -1,4 +1,7 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using OPJosMod.Patches;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +17,24 @@ namespace OPJosMod
         private const string modName = "OpJosMod";
         private const string modVersion = "1.0.0.0";
 
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static OpJosMod Instance;
 
+        internal ManualLogSource mls;
 
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("mod has started");
+
+            harmony.PatchAll(typeof(OpJosMod));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
     }
 }
