ff7b5748ab5c6d03670c5e51b03f80486635d9dc
Jglauby <jglauby@gmail.com>
2024-01-08 20:55:11 -0600

    tuned some numbers, cut out stuff i didn't need

--- OPJosMod/Patches/PlayerControllerBPatch.cs
+++ OPJosMod/Patches/PlayerControllerBPatch.cs
@@ -24,7 +24,12 @@ namespace OPJosMod.BetterStamina.Patches
         [HarmonyPatch("Update")]
         [HarmonyPostfix]
         static void patchUpdate(PlayerControllerB __instance)
-        {            
+        {
+            __instance.sprintTime = 19f; //default 5
+
+            float walkingSprintRegen = 22f; 
+            float standingSprintRegen = 13f; 
+
             FieldInfo isWalkingField = typeof(PlayerControllerB).GetField("isWalking", BindingFlags.NonPublic | BindingFlags.Instance);
 
             if (isWalkingField != null)
@@ -36,32 +41,17 @@ namespace OPJosMod.BetterStamina.Patches
                 {
                     __instance.localVisor.position = __instance.localVisorTargetPoint.position;
                     __instance.localVisor.rotation = Quaternion.Lerp(__instance.localVisor.rotation, __instance.localVisorTargetPoint.rotation, 53f * Mathf.Clamp(Time.deltaTime, 0.0167f, 20f));
+                    
                     float num2 = 1f;
-                    if (__instance.drunkness > 0.02f)
-                    {
-                        num2 *= Mathf.Abs(StartOfRound.Instance.drunknessSpeedEffect.Evaluate(__instance.drunkness) - 1.25f);
-                    }
-                    if (__instance.isSprinting)
-                    {
-                        __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter - Time.deltaTime / __instance.sprintTime * __instance.carryWeight * num2, 0f, 1f);
-                        mls.LogMessage("BetterStamina(error): adjusting spritn meter");
-                    }
-                    else if (__instance.isMovementHindered > 0)
-                    {
-                        if (isWalkingValue)
-                        {
-                            __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter - Time.deltaTime / __instance.sprintTime * num2 * 0.5f, 0f, 1f);
-                        }
-                    }
-                    else
+                    if (!__instance.isSprinting && __instance.isMovementHindered <= 0)
                     {
                         if (!isWalkingValue)
                         {
-                            __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter + Time.deltaTime / (__instance.sprintTime + 4f) * num2, 0f, 1f);
+                            __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter + Time.deltaTime / standingSprintRegen * num2, 0f, 1f);
                         }
                         else
                         {
-                            __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter + Time.deltaTime / (__instance.sprintTime + 9f) * num2, 0f, 1f);
+                            __instance.sprintMeter = Mathf.Clamp(__instance.sprintMeter + Time.deltaTime / walkingSprintRegen * num2, 0f, 1f);
                         }
                         if (__instance.isExhausted && __instance.sprintMeter > 0.2f)
                         {
