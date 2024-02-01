217786551b3b07bec7b25cb4f3073d58243d56e1
Jglauby <jglauby@gmail.com>
2024-02-01 14:06:55 -0600

    can hear everyone at all distances

--- OPJosMod/Patches/StartOfRoundPatch.cs
+++ OPJosMod/Patches/StartOfRoundPatch.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx.Logging;
+using Dissonance.Integrations.Unity_NFGO;
 using DunGen;
 using GameNetcodeStuff;
 using HarmonyLib;
@@ -46,6 +47,46 @@ namespace OPJosMod.GhostMode.Patches
             mls.LogMessage("player connected patch hit in start of round class");
 
             PlayerControllerBPatch.resetGhostModeVars(__instance.localPlayerController);
+        }
+
+        [HarmonyPatch("UpdatePlayerVoiceEffects")]
+        [HarmonyPrefix]
+        static void updatePlayerVoiceEffectsDead(PlayerControllerB __instance)
+        {
+            mls.LogMessage("UpdatePlayerVoiceEffects post hit");
+
+            for (int i = 0; i < StartOfRound.Instance.allPlayerScripts.Length; i++)
+            {
+                PlayerControllerB playerControllerB2 = StartOfRound.Instance.allPlayerScripts[i];
+
+                // Check if the player is the local player or controlled by the local player
+                if (playerControllerB2 == GameNetworkManager.Instance.localPlayerController)
+                {
+                    // Skip the local player
+                    continue;
+                }
+
+                // Check if the player's voice chat objects are accessible
+                if (playerControllerB2.voicePlayerState == null || playerControllerB2.currentVoiceChatIngameSettings._playerState == null || playerControllerB2.currentVoiceChatAudioSource == null)
+                {
+                    // Attempt to refresh voice chat objects
+                    StartOfRound.Instance.RefreshPlayerVoicePlaybackObjects();
+                    if (playerControllerB2.voicePlayerState == null || playerControllerB2.currentVoiceChatAudioSource == null)
+                    {
+                        mls.LogMessage($"Was not able to access voice chat object for player #{i}; {playerControllerB2.voicePlayerState == null}; {playerControllerB2.currentVoiceChatAudioSource == null}");
+                        continue;
+                    }
+                }
+
+                // Adjust audio properties for all players
+                mls.LogMessage($"adjusting volume for player{i}");
+                AudioSource currentVoiceChatAudioSource = StartOfRound.Instance.allPlayerScripts[i].currentVoiceChatAudioSource;
+                currentVoiceChatAudioSource.spatialBlend = 0f;
+                playerControllerB2.currentVoiceChatIngameSettings.set2D = true;
+                currentVoiceChatAudioSource.volume = 1f;
+
+                throw new Exception("dont call the regular UpdatePlayerVoiceEffects function");
+            }
         }
     }
 }
