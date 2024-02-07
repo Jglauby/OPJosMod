447ea1fc0a7011e5e38ef75aa8f03503cbc6b962
Jglauby <jglauby@gmail.com>
2024-02-07 17:01:09 -0600

    untest function

--- OPJosMod/Patches/PlayerControllerBPatch.cs
+++ OPJosMod/Patches/PlayerControllerBPatch.cs
@@ -4,6 +4,7 @@ using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,7 +23,28 @@ namespace OPJosMod.Patches
         [HarmonyPostfix]
         static void patchUpdate(PlayerControllerB __instance)
         {
-            
+
+            if (__instance.isSprinting)
+            {
+                FieldInfo sprintMultiplierField = typeof(PlayerControllerB).GetField("sprintMultiplier", BindingFlags.NonPublic | BindingFlags.Instance);
+                if (sprintMultiplierField != null)
+                {
+                    var currentValue = sprintMultiplierField.GetValue(__instance);
+                    if (currentValue is float)
+                    {
+                        var newForce = (float)currentValue * 1.05f;
+                        sprintMultiplierField.SetValue(__instance, newForce);
+                    }
+                    else
+                    {
+                        mls.LogError("current spritnMultiplier isn't a float?");
+                    }
+                }
+                else
+                {
+                    mls.LogError("private field not found");
+                }
+            }
         }
     }
 }
