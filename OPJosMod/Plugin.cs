0c86b1cc44d23589e58962911bd5d69d87f8972e
Jglauby <jglauby@gmail.com>
2024-02-23 17:23:31 -0600

    added setupconfig function

--- OPJosMod/Plugin.cs
+++ OPJosMod/Plugin.cs
@@ -33,6 +33,7 @@ namespace OPJosMod.GhostMode
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
 
             mls.LogInfo("mod has started");
+            setupConfig();
 
             PlayerControllerBPatch.SetLogSource(mls);
             StartOfRoundPatch.SetLogSource(mls);
@@ -53,6 +54,23 @@ namespace OPJosMod.GhostMode
             harmony.PatchAll(typeof(ForestGiantAIPatch));
             harmony.PatchAll(typeof(SandSpiderAIPatch));
             harmony.PatchAll(typeof(NutcrackerEnemyAIPatch));
+        }
+
+        private void setupConfig()
+        {
+            var configSprintMultiplier = Config.Bind("Sprint Multiplier", // The section under which the option is shown
+                                        "SprintMultiplier",  // The key of the configuration option in the configuration file
+                                        1.04f, // The default value
+                                        "How fast your speed rams up when sprinting"); // Description of the option to show in the config file
+
+
+            var configFlashTimeButton = Config.Bind("Flash Time Button",
+                                        "FlashTimeButton",
+                                        "R",
+                                        "Button used to toggle flash time");
+
+            ConfigVariables.defaultSprintMultiplier = configSprintMultiplier.Value;
+            ConfigVariables.flashTimeButton = configFlashTimeButton.Value;
         }
     }
 }
