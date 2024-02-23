0a2f28c05cbf6ae6255cdd8350dbb7a70b461fe6
Jglauby <jglauby@gmail.com>
2024-02-23 17:22:37 -0600

    added the config variables class

---
+++ OPJosMod/ConfigVariables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine.InputSystem;
+
+namespace OPJosMod
+{
+    public static class ConfigVariables
+    {
+        public static float defaultSprintMultiplier;
+        public static string flashTimeButton;
+
+        public static Key getFlashTimeButton()
+        {
+            Key key = (Key)Enum.Parse(typeof(Key), flashTimeButton);
+            return key;
+        }
+    }
+}
