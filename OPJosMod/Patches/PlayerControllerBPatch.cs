6b5f5253ce7ef8d40c19991d95e346db66565d89
Jglauby <jglauby@gmail.com>
2024-03-02 02:53:49 -0600

    seeker is locked in place when level starts for a ceratain amount of time

--- OPJosMod/Patches/PlayerControllerBPatch.cs
+++ OPJosMod/Patches/PlayerControllerBPatch.cs
@@ -21,6 +21,7 @@ namespace OPJosMode.HideNSeek.Patches
         public static bool isHider = false;
 
         private static Coroutine teleportCoroutine;
+        private static Coroutine lockPlayerCoroutine;
 
         public static void SetupHider()
         {
@@ -28,6 +29,9 @@ namespace OPJosMode.HideNSeek.Patches
             PlayerControllerB localPlayerController = StartOfRound.Instance.localPlayerController;
             isSeeker = false;
             isHider = true;
+
+            //do nothing for a couple seconds
+            lockPlayerCoroutine = localPlayerController.StartCoroutine(lockPlayer(localPlayerController, 2f));
 
             //set suit to green?
 
@@ -43,6 +47,7 @@ namespace OPJosMode.HideNSeek.Patches
             isSeeker = true;
             isHider = false;
 
+            lockPlayerCoroutine = localPlayerController.StartCoroutine(lockPlayer(localPlayerController, 15f));
             //increase speed slightly
             //give gun
             //remove scan option
@@ -56,9 +61,25 @@ namespace OPJosMode.HideNSeek.Patches
                 player.StopCoroutine(teleportCoroutine);
             }
 
-            yield return new WaitForSeconds(5f);
+            yield return new WaitForSeconds(0.1f);
 
             GameNetworkManager.Instance.localPlayerController.TeleportPlayer(location);
+        }
+
+        private static IEnumerator lockPlayer(PlayerControllerB player, float lockTime)
+        {
+            if (lockPlayerCoroutine != null)
+            {
+                player.StopCoroutine(lockPlayerCoroutine);
+            }
+
+            yield return new WaitForSeconds(0.1f);
+            mls.LogMessage("player locked in place");
+            player.playerCollider.enabled = false;
+
+            yield return new WaitForSeconds(lockTime);
+            mls.LogMessage("player unlocked!");
+            player.playerCollider.enabled = true;
         }
     }
 }
