7459e2576fa44c44890740d548bbbfae9cdbcef0
Jglauby <jglauby@gmail.com>
2024-03-05 01:36:15 -0600

    small change  to comment  hint

--- OPJosMod/Patches/PlayerControllerBPatch.cs
+++ OPJosMod/Patches/PlayerControllerBPatch.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OPJosMod.Patches//OPJosMode.MODNameHere.Patches
+namespace OPJosMod.Patches//OPJosMod.MODNameHere.Patches
 {
     [HarmonyPatch(typeof(PlayerControllerB))]
     internal class PlayerControllerBPatch
