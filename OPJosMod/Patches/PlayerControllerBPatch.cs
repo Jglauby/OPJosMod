53bb0ce03f07069cfafc28a9961e1b3b2bb5f9be
Jglauby <jglauby@gmail.com>
2024-03-21 14:45:57 -0500

    improved the teleporting back logic, only adds a safe location when you are on the ground

--- OPJosMod/Patches/PlayerControllerBPatch.cs
+++ OPJosMod/Patches/PlayerControllerBPatch.cs
@@ -31,6 +31,13 @@ namespace OPJosMod.LagJutsu.Patches
         private static float lastTimeAddedLocation = Time.time;
         private static List<Vector3> lastSafeLocations = new List<Vector3>();
 
+        [HarmonyPatch("Start")]
+        [HarmonyPrefix]
+        static void startPatch(PlayerControllerB __instance)
+        {
+            lastSafeLocations.Add(RoundManager.Instance.playersManager.playerSpawnPositions[0].transform.position);
+        }
+
         [HarmonyPatch("Update")]
         [HarmonyPrefix]
         static void patchUpdate(PlayerControllerB __instance)
@@ -40,13 +47,13 @@ namespace OPJosMod.LagJutsu.Patches
                 handleGodModeToggle();
 
                 //handle saving last locations you were safe at
-                if (Time.time - lastTimeDied > ConfigVariables.RewindBackTimeSeconds && Time.time - lastTimeAddedLocation > ConfigVariables.RewindBackTimeSeconds)
+                if (Time.time - lastTimeDied > 1 && Time.time - lastTimeAddedLocation > 1 && __instance.thisController.isGrounded)
                 {
-                    mls.LogMessage($"time.time:{Time.time} lastTimeDied:{lastTimeDied} rwindBackTimeSeconds:{ConfigVariables.RewindBackTimeSeconds}");
+                    //mls.LogMessage($"time.time:{Time.time} lastTimeDied:{lastTimeDied} rwindBackTimeSeconds:{ConfigVariables.RewindBackTimeSeconds}");
                     lastTimeAddedLocation = Time.time;
 
                     //remove earliest in list if listsize is at max
-                    if (lastSafeLocations.Count >= ConfigVariables.RewindBackTimeSeconds * 5)
+                    if (lastSafeLocations.Count >= ConfigVariables.RewindBackTimeSeconds * 10)
                         lastSafeLocations.RemoveAt(0);
 
                     //dont save the last safe location if it is basically the same spot as the last one that was saved
@@ -84,17 +91,22 @@ namespace OPJosMod.LagJutsu.Patches
             if (lastSafeLocations.Count > 0)
             {
                 Vector3 newLocation = new Vector3();
-                if (Time.time - lastTimeDied > ConfigVariables.RewindBackTimeSeconds)
+                if (lastSafeLocations.Count > ConfigVariables.RewindBackTimeSeconds)
+                {
+                    newLocation = lastSafeLocations[lastSafeLocations.Count - ConfigVariables.RewindBackTimeSeconds];
+                    lastSafeLocations.RemoveAt(lastSafeLocations.Count - ConfigVariables.RewindBackTimeSeconds);
+                }
+                else if (lastSafeLocations.Count > 0)
+                {
                     newLocation = lastSafeLocations[lastSafeLocations.Count - 1];
-                else if (lastSafeLocations.Count > 1)
-                    newLocation = lastSafeLocations[lastSafeLocations.Count - 2];
+                    lastSafeLocations.RemoveAt(lastSafeLocations.Count - 1);
+                }
                 else
+                {
                     newLocation = lastSafeLocations[0];
-
+                }
 
                 StartOfRound.Instance.localPlayerController.transform.position = newLocation;
-                lastSafeLocations.RemoveAt(lastSafeLocations.Count - 1);
-
                 mls.LogMessage($"teleport player to: {newLocation}");
             }
         }
