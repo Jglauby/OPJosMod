6ff4da68e9603ba4d156e6f60f6b125adadf0c6f
Jglauby <jglauby@gmail.com>
2024-04-25 14:04:44 -0500

    Revert "remove custom rpc as its not needed"
    This reverts commit 862a539413eaa94a6d76d9a0c26f6ba2027554da.

---
+++ OPJosMod/CustomRpc/MessageTasks.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace OPJosMod.ReviveCompany.CustomRpc
+{
+    public enum MessageTasks 
+    {
+        ModActivated,
+        RevivePlayer,
+        TurnOffGhostMode,
+        ErrorNoTask
+    }
+
+    public static class MessageTaskUtil
+    {
+        public static string GetCode(MessageTasks code)
+        {
+            switch (code)
+            {
+                case MessageTasks.ModActivated:
+                    return ":ModActivated:";
+                case MessageTasks.RevivePlayer:
+                    return ":RevivePlayer:";
+                case MessageTasks.TurnOffGhostMode:
+                    return ":TurnOffGhostMode:";
+                case MessageTasks.ErrorNoTask:
+                    return ":Error:";
+            }
+
+            return ":Error:";
+        }
+
+        public static string getMessageWithoutTask(string message)
+        {
+            foreach (MessageTasks task in Enum.GetValues(typeof(MessageTasks)))
+            {
+                string code = GetCode(task);
+                message = message.Replace(code, "");
+            }
+
+            return message.Trim();
+        }
+
+        public static MessageTasks getMessageTask(string givenString)
+        {
+            if (givenString.Contains(GetCode(MessageTasks.ModActivated)))
+            {
+                return MessageTasks.ModActivated;
+            }
+            else if (givenString.Contains(GetCode(MessageTasks.RevivePlayer))){
+                return MessageTasks.RevivePlayer;
+            }
+            else if (givenString.Contains(GetCode(MessageTasks.TurnOffGhostMode)))
+            {
+                return MessageTasks.TurnOffGhostMode;
+            }
+            else
+            {
+                return MessageTasks.ErrorNoTask;
+            }
+        }
+    }
+}
---
+++ OPJosMod/CustomRpc/RpcMessage.cs
@@ -0,0 +1,27 @@
+﻿namespace OPJosMod.ReviveCompany.CustomRpc
+{
+    public class RpcMessage
+    {
+        public RpcMessage(MessageTasks task, string message, int user, MessageCodes code)
+        {
+            Task = task;
+            Message = message;
+            FromUser = user;
+            MessageCode = code;
+        }
+
+        public MessageTasks Task { get; set; }
+
+        public string Message { get; set; }
+
+        public int FromUser { get; set; }
+
+        public MessageCodes MessageCode { get; set; }
+
+        public string getMessageWithCode()
+        {
+            string result = MessageCodeUtil.GetCode(MessageCode) + MessageTaskUtil.GetCode(Task) + Message;
+            return result;
+        }
+    }
+}
